7038a0fbc3c03b6fc8ca78a88b89d10db59b8808
Flashdranzer <ponukumati.deepak@gmail.com>
2023-09-04 05:22:51 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/wodenvalleyalliancechurch/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wodenvalleyalliancechurch.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:woden_valley_alliance_church/core/app_export.dart';
+
+class ApiClient {}
